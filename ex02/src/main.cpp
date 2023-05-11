be510ccf6fd39826edb69d530cfaaa7f90fda5ac
Saphirac <mimiforspam1697@gmail.com>
2023-05-11 05:39:03 +0200

    Ex02 [OK]

---
+++ ex02/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 05:28:46 by mcourtoi          #+#    #+#             */
+/*   Updated: 2023/05/11 05:38:29 by mcourtoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string	test = "/"HI THIS IS BRAIN\"";
+	std::string	*stringPTR = &test;
+	std::string	&stringREF = test;
+
+	std::cout	<< 	"&test : " << &test << '\n';
+	std::cout	<< 	"stringPTR : " << stringPTR << '\n';
+	std::cout	<< 	"&stringREF : " << &stringREF << '\n';
+
+	std::cout	<< 	"test : " << test << '\n';
+	std::cout	<< 	"*stringPTR : " << *stringPTR << '\n';
+	std::cout	<< 	"stringREF : " << stringREF << '\n';
+}
